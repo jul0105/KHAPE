41de656e783d113a0cfe79bc3b1fa8ccfdc21287
Julien Béguin <julien.beguin99@gmail.com>
2021-11-23 22:34:03 +0100

    wip oprf

---
+++ src/oprf.rs
@@ -0,0 +1,38 @@
+use rand::{thread_rng, Rng};
+use rand::rngs::OsRng;
+use voprf::{NonVerifiableClient, NonVerifiableClientBlindResult, NonVerifiableServer, NonVerifiableServerEvaluateResult};
+use curve25519_dalek::digest::generic_array::GenericArray;
+
+type Group = curve25519_dalek::ristretto::RistrettoPoint;
+type Hash = sha2::Sha512;
+
+pub fn generate_secret() -> [u8; 32] {
+    thread_rng().gen()
+}
+
+pub fn client_init(password: &[u8]) -> NonVerifiableClientBlindResult<Group, Hash> {
+    let mut client_rng = OsRng;
+
+    NonVerifiableClient::<Group, Hash>::blind(
+        password.to_vec(),
+        &mut client_rng,
+    ).expect("Unable to construct client")
+}
+
+pub fn server_evaluate(client_blind_result: NonVerifiableClientBlindResult<Group, Hash>) -> NonVerifiableServerEvaluateResult<Group, Hash> {
+    let secret_salt = generate_secret();
+    let server = NonVerifiableServer::<Group, Hash>::new_with_key(&secret_salt)
+        .expect("Unable to construct server");
+
+    server.evaluate(
+        client_blind_result.message,
+        None,
+    ).expect("Unable to perform server evaluate")
+}
+
+pub fn client_finish(client_blind_result: NonVerifiableClientBlindResult<Group, Hash>, server_evaluate_result: NonVerifiableServerEvaluateResult<Group, Hash>) -> Vec<u8> {
+    client_blind_result.state.finalize(
+        server_evaluate_result.message,
+        None,
+    ).expect("Unable to perform client finalization").to_vec()
+}
--- src/try_oprf.rs
+++ src/try_oprf.rs
@@ -1,5 +1,6 @@
 use voprf::{NonVerifiableServer, NonVerifiableClient};
 use rand::{rngs::OsRng, RngCore};
+use crate::oprf::*;
 
 type Group = curve25519_dalek::ristretto::RistrettoPoint;
 type Hash = sha2::Sha512;
@@ -10,13 +11,6 @@ mod tests {
 
     #[test]
     pub fn try_oprf_fn() {
-        // Server
-        let mut server_rng = OsRng;
-        let server = NonVerifiableServer::<Group, Hash>::new(&mut server_rng)
-            .expect("Unable to construct server");
-
-        // println!("{:?}", server.get_private_key());
-
         // Client
         let mut client_rng = OsRng;
         let client_blind_result = NonVerifiableClient::<Group, Hash>::blind(
@@ -25,6 +19,10 @@ mod tests {
         ).expect("Unable to construct client");
 
         // Server
+        let secret_salt = generate_secret();
+        let server = NonVerifiableServer::<Group, Hash>::new_with_key(&secret_salt)
+            .expect("Unable to construct server");
+
         let server_evaluate_result = server.evaluate(
             client_blind_result.message,
             None,
@@ -49,7 +47,10 @@ mod tests {
         ).expect("Unable to construct client");
 
         // Server
-        let server_evaluate_result = server.evaluate(
+        let server2 = NonVerifiableServer::<Group, Hash>::new_with_key(&secret_salt)
+            .expect("Unable to construct server");
+
+        let server_evaluate_result = server2.evaluate(
             client_blind_result.message,
             None,
         ).expect("Unable to perform server evaluate");
@@ -61,6 +62,8 @@ mod tests {
         ).expect("Unable to perform client finalization");
 
         println!("VOPRF output: {:?}", client_finalize_result2.to_vec());
+        println!("VOPRF secret salt: {:?}", secret_salt);
+
 
         assert_eq!(client_finalize_result, client_finalize_result2);
 
