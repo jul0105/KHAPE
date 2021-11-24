49e85804b46bfc5e6fd0bb6d47fa758eb14d3958
Julien Béguin <julien.beguin99@gmail.com>
2021-11-24 11:58:44 +0100

    OPRF serialization

--- src/oprf.rs
+++ src/oprf.rs
@@ -10,28 +10,30 @@ pub fn generate_secret() -> [u8; 32] {
     thread_rng().gen()
 }
 
-pub fn client_init(password: &[u8]) -> NonVerifiableClientBlindResult<Group, Hash> {
+pub fn client_init(password: &[u8]) -> (NonVerifiableClient<Group, Hash>, Vec<u8>) {
     let mut client_rng = OsRng;
 
-    NonVerifiableClient::<Group, Hash>::blind(
+    let result = NonVerifiableClient::<Group, Hash>::blind(
         password.to_vec(),
         &mut client_rng,
-    ).expect("Unable to construct client")
+    ).expect("Unable to construct client");
+
+    (result.state, result.message.serialize())
 }
 
-pub fn server_evaluate(client_blind_result: BlindedElement<Group, Hash>, secret_salt: [u8; 32]) -> EvaluationElement<Group, Hash> {
+pub fn server_evaluate(client_blind_result: Vec<u8>, secret_salt: [u8; 32]) -> Vec<u8> {
     let server = NonVerifiableServer::<Group, Hash>::new_with_key(&secret_salt)
         .expect("Unable to construct server");
 
     server.evaluate(
-        client_blind_result,
+        BlindedElement::<Group, Hash>::deserialize(&client_blind_result).unwrap(),
         None,
-    ).expect("Unable to perform server evaluate").message
+    ).expect("Unable to perform server evaluate").message.serialize()
 }
 
-pub fn client_finish(client_blind_result: NonVerifiableClientBlindResult<Group, Hash>, server_evaluate_result: EvaluationElement<Group, Hash>) -> Vec<u8> {
-    client_blind_result.state.finalize(
-        server_evaluate_result,
+pub fn client_finish(client_state: NonVerifiableClient<Group, Hash>, server_evaluate_result: Vec<u8>) -> Vec<u8> {
+    client_state.finalize(
+        EvaluationElement::<Group, Hash>::deserialize(&server_evaluate_result).unwrap(),
         None,
     ).expect("Unable to perform client finalization").to_vec()
 }
@@ -45,17 +47,17 @@ mod tests {
         let password = b"input";
 
         // Client
-        let client_blind_result = client_init(password);
-        println!("VOPRF blind result: {:?}", client_blind_result.message.serialize());
+        let (client_state, client_blind_result) = client_init(password);
+        println!("VOPRF blind result: {:?}", client_blind_result);
 
         // Server
         let secret_salt = generate_secret();
-        let server_evaluate_result = server_evaluate(client_blind_result.message.clone(), secret_salt);
+        let server_evaluate_result = server_evaluate(client_blind_result, secret_salt);
         println!("VOPRF secret salt: {:?}", secret_salt);
 
         // Client
-        let client_finalize_result = client_finish(client_blind_result, server_evaluate_result);
-        println!("VOPRF output: {:?}", client_finalize_result.to_vec());
+        let client_finalize_result = client_finish(client_state, server_evaluate_result);
+        println!("VOPRF output: {:?}", client_finalize_result);
 
 
         println!();
@@ -63,16 +65,16 @@ mod tests {
 
 
         // Client
-        let client_blind_result2 = client_init(password);
-        println!("VOPRF blind result: {:?}", client_blind_result2.message.serialize());
+        let (client_state2, client_blind_result2) = client_init(password);
+        println!("VOPRF blind result: {:?}", client_blind_result2);
 
         // Server
-        let server_evaluate_result2 = server_evaluate(client_blind_result2.message.clone(), secret_salt);
+        let server_evaluate_result2 = server_evaluate(client_blind_result2, secret_salt);
         println!("VOPRF secret salt: {:?}", secret_salt);
 
         // Client
-        let client_finalize_result2 = client_finish(client_blind_result2, server_evaluate_result2);
-        println!("VOPRF output: {:?}", client_finalize_result2.to_vec());
+        let client_finalize_result2 = client_finish(client_state2, server_evaluate_result2);
+        println!("VOPRF output: {:?}", client_finalize_result2);
 
         assert_eq!(client_finalize_result, client_finalize_result2);
     }
