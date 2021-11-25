0973576d0f5791b5e6339cc7aea835f8d8623905
Julien Béguin <julien.beguin99@gmail.com>
2021-11-25 16:36:42 +0100

    implement 'ideal cipher' with feistel

--- src/ideal_cipher.rs
+++ src/ideal_cipher.rs
@@ -1,4 +1,4 @@
-use sha3::Digest;
+use sha3::{Sha3_256, Digest};
 use std::convert::TryFrom;
 
 const INPUT_SIZE: usize = 64;
@@ -12,20 +12,20 @@ pub fn encrypt_feistel(key: [u8; 32], plaintext: [u8; INPUT_SIZE]) -> [u8; INPUT
     let mut right_part: [u8; 32] = <[u8; 32]>::try_from(&plaintext[32..64]).unwrap();
 
     // N-1 feistel round
-    for i in 0..NB_FEISTEL_ROUND {
+    for i in 0..(NB_FEISTEL_ROUND - 1) {
         let result = feistel_round(key, [i as u8], left_part, right_part);
         left_part = right_part;
         right_part = result;
     }
 
     // Final round (without mix)
-    left_part = feistel_round(key, [NB_FEISTEL_ROUND as u8], left_part, right_part);
+    left_part = feistel_round(key, [(NB_FEISTEL_ROUND - 1) as u8], left_part, right_part);
 
     <[u8; 64]>::try_from([left_part, right_part].concat()).unwrap()
 }
 
 fn feistel_round(key: [u8; 32], round_nb: [u8; 1], left_part: [u8; 32], right_part: [u8; 32]) -> [u8; 32] {
-    let hash_result: [u8; 32] = <[u8; 32]>::from(sha3::Sha3_256::new()
+    let hash_result: [u8; 32] = <[u8; 32]>::from(Sha3_256::new()
         .chain(key)
         .chain(round_nb)
         .chain(right_part)
@@ -40,14 +40,14 @@ pub fn decrypt_feistel(key: [u8; 32], ciphertext: [u8; INPUT_SIZE]) -> [u8; INPU
     let mut left_part: [u8; 32] = <[u8; 32]>::try_from(&ciphertext[32..64]).unwrap();
 
     // N-1 feistel round
-    for i in NB_FEISTEL_ROUND..0 {
+    for i in (1..NB_FEISTEL_ROUND).rev() {
         let result = feistel_round(key, [i as u8], right_part, left_part);
         right_part = left_part;
         left_part = result;
     }
 
     // Final round (without mix)
-    right_part = feistel_round(key, [0u8], left_part, right_part);
+    right_part = feistel_round(key, [0u8], right_part, left_part);
 
     <[u8; 64]>::try_from([left_part, right_part].concat()).unwrap()
 }
