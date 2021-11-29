e434f39f12ac5751eeadcf18d8300c7cc34dd641
Julien Béguin <julien.beguin99@gmail.com>
2021-11-29 15:07:03 +0100

    modify private key generation to rejection method

--- src/group.rs
+++ src/group.rs
@@ -7,20 +7,21 @@ use crate::khape::{PublicKey, PrivateKey, RawPublicKey};
 
 const SIGN: u8 = 0; // TODO elligator sign
 
-fn compute_private_key(bytes: [u8; 32]) -> PrivateKey {
-    clamp_scalar(bytes)
-}
-
 fn compute_public_key(private_key: PrivateKey) -> RawPublicKey {
     X25519_BASEPOINT * private_key
 }
 
 pub fn compute_shared_key(own_private_key: [u8; 32], opposing_public_key: [u8; 32]) -> [u8; 32] {
-    (clamp_scalar(own_private_key) * encode_public_key(&FieldElement::from_bytes(&opposing_public_key))).to_bytes() // From x25519-dalek library
+    (Scalar::from_bits(own_private_key) * encode_public_key(&FieldElement::from_bytes(&opposing_public_key))).to_bytes() // From x25519-dalek library
 }
 
 fn generate_private_key() -> PrivateKey {
-    clamp_scalar(thread_rng().gen::<[u8; 32]>())
+    loop {
+        let private_key_candidate = Scalar::from_canonical_bytes(thread_rng().gen::<[u8; 32]>());
+        if private_key_candidate.is_some() {
+            return private_key_candidate.unwrap();
+        }
+    }
 }
 
 pub fn generate_keys() -> (PrivateKey, PublicKey) { // TODO try to return FieldElement instead of [u8; 32]
@@ -41,13 +42,6 @@ fn encode_public_key(field_element: &PublicKey) -> RawPublicKey {
     elligator_encode(field_element)
 }
 
-fn clamp_scalar(mut scalar: [u8; 32]) -> Scalar { // From x25519-dalek library
-    scalar[0] &= 248;
-    scalar[31] &= 127;
-    scalar[31] |= 64;
-
-    Scalar::from_bits(scalar)
-}
 
 #[cfg(test)]
 mod tests {
@@ -60,5 +54,19 @@ mod tests {
         let public_key_elligator2 = decode_public_key(&public_key);
 
         assert_eq!(public_key_elligator, public_key_elligator2);
+    }
+
+    #[test]
+    fn test_generate_private_key_with_rejection_method() {
+        for i in 0..1000 {
+            let random_bytes = thread_rng().gen::<[u8; 32]>();
+            let a = Scalar::from_canonical_bytes(random_bytes);
+            if a.is_some() {
+                println!("Success on try {}", i);
+                return;
+            } else {
+                println!("try {}", i);
+            }
+        }
     }
 }
