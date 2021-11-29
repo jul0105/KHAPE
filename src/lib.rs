f8645995f5d1326bebe0bbea25ef3f39ca63688b
Julien Béguin <julien.beguin99@gmail.com>
2021-11-29 17:43:42 +0100

    modify main.rs to lib.rs and expose API

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,14 @@ mod hash;
 mod prf;
 mod ideal_cipher;
 
-fn main() {
+// Client functions
+pub use crate::khape::{client_register_start, client_register_finish, client_auth_start, client_auth_ke, client_auth_finish};
+// Server functions
+pub use crate::khape::{server_register_start, server_register_finish, server_auth_start, server_auth_finish};
 
-}
+// Register messages
+pub use crate::khape::{RegisterRequest, RegisterResponse, RegisterFinish};
+// Login messages
+pub use crate::khape::{AuthRequest, AuthResponse, AuthVerifyRequest, AuthVerifyResponse};
+
+pub use crate::khape::{OutputKey, OprfClientState, FileEntry, EphemeralKeys, PreRegisterSecrets};
