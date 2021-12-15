621e1bad82a97ea3a020136ce4f1846b8b281495
Julien Béguin <julien.beguin99@gmail.com>
2021-12-15 15:54:07 +0100

    add consts

--- src/ideal_cipher.rs
+++ src/ideal_cipher.rs
@@ -1,16 +1,16 @@
 use sha3::{Sha3_256, Digest};
 use std::convert::TryFrom;
 
-pub const KEY_SIZE: usize = 32;
-const INPUT_SIZE: usize = 64;
-const PART_SIZE: usize = INPUT_SIZE/2;
+const CIPHER_SIZE: usize = 64;
+const PART_SIZE: usize = CIPHER_SIZE / 2;
+pub const KEY_SIZE: usize = PART_SIZE;
 const NB_FEISTEL_ROUND: usize = 14;
 
 
-pub(crate) fn encrypt_feistel(key: [u8; 32], plaintext: [u8; INPUT_SIZE]) -> [u8; INPUT_SIZE] {
+pub(crate) fn encrypt_feistel(key: [u8; KEY_SIZE], plaintext: [u8; CIPHER_SIZE]) -> [u8; CIPHER_SIZE] {
     // Split plaintext in 2 equal part L0 and R0
-    let mut left_part: [u8; 32] = <[u8; 32]>::try_from(&plaintext[0..32]).unwrap();
-    let mut right_part: [u8; 32] = <[u8; 32]>::try_from(&plaintext[32..64]).unwrap();
+    let mut left_part: [u8; PART_SIZE] = <[u8; PART_SIZE]>::try_from(&plaintext[0..PART_SIZE]).unwrap();
+    let mut right_part: [u8; PART_SIZE] = <[u8; PART_SIZE]>::try_from(&plaintext[PART_SIZE..CIPHER_SIZE]).unwrap();
 
     // N-1 feistel round
     for i in 0..(NB_FEISTEL_ROUND - 1) {
@@ -22,13 +22,13 @@ pub(crate) fn encrypt_feistel(key: [u8; 32], plaintext: [u8; INPUT_SIZE]) -> [u8
     // Final round (without mix)
     left_part = feistel_round(key, [(NB_FEISTEL_ROUND - 1) as u8], left_part, right_part);
 
-    <[u8; 64]>::try_from([left_part, right_part].concat()).unwrap()
+    <[u8; CIPHER_SIZE]>::try_from([left_part, right_part].concat()).unwrap()
 }
 
-pub(crate) fn decrypt_feistel(key: [u8; 32], ciphertext: [u8; INPUT_SIZE]) -> [u8; INPUT_SIZE] {
+pub(crate) fn decrypt_feistel(key: [u8; KEY_SIZE], ciphertext: [u8; CIPHER_SIZE]) -> [u8; CIPHER_SIZE] {
     // Split plaintext in 2 equal part Ln+1 and Rn+1
-    let mut right_part: [u8; 32] = <[u8; 32]>::try_from(&ciphertext[0..32]).unwrap();
-    let mut left_part: [u8; 32] = <[u8; 32]>::try_from(&ciphertext[32..64]).unwrap();
+    let mut right_part: [u8; PART_SIZE] = <[u8; PART_SIZE]>::try_from(&ciphertext[0..PART_SIZE]).unwrap();
+    let mut left_part: [u8; PART_SIZE] = <[u8; PART_SIZE]>::try_from(&ciphertext[PART_SIZE..CIPHER_SIZE]).unwrap();
 
     // N-1 feistel round
     for i in (1..NB_FEISTEL_ROUND).rev() {
@@ -40,17 +40,17 @@ pub(crate) fn decrypt_feistel(key: [u8; 32], ciphertext: [u8; INPUT_SIZE]) -> [u
     // Final round (without mix)
     right_part = feistel_round(key, [0u8], right_part, left_part);
 
-    <[u8; 64]>::try_from([right_part, left_part].concat()).unwrap()
+    <[u8; CIPHER_SIZE]>::try_from([right_part, left_part].concat()).unwrap()
 }
 
-fn feistel_round(key: [u8; 32], round_nb: [u8; 1], left_part: [u8; 32], right_part: [u8; 32]) -> [u8; 32] {
-    let hash_result: [u8; 32] = <[u8; 32]>::from(Sha3_256::new()
+fn feistel_round(key: [u8; KEY_SIZE], round_nb: [u8; 1], left_part: [u8; PART_SIZE], right_part: [u8; PART_SIZE]) -> [u8; PART_SIZE] {
+    let hash_result: [u8; PART_SIZE] = <[u8; PART_SIZE]>::from(Sha3_256::new()
         .chain(key)
         .chain(round_nb)
         .chain(right_part)
         .finalize());
     let result: Vec<u8> = left_part.iter().zip(hash_result.iter()).map(|(&a, &b)| a ^ b).collect();
-    <[u8; 32]>::try_from(result).unwrap()
+    <[u8; PART_SIZE]>::try_from(result).unwrap()
 }
 
 
