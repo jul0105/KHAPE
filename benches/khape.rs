fc2f9b5e3bf0d56bcf17887e6308d0f366752464
Julien Béguin <julien.beguin99@gmail.com>
2021-12-18 16:16:53 +0100

    fix benchmark value clone

--- benches/khape.rs
+++ benches/khape.rs
@@ -6,9 +6,9 @@ const USE_OPRF: bool = false;
 const USE_SLOW_HASH: bool = false;
 
 pub fn client_initialization(c: &mut Criterion) {
-    let uid = String::from("username");
+    let uid = "username";
 
-    c.bench_function("client_initialization", |b| b.iter(|| Client::new(Parameters::new(USE_OPRF, USE_SLOW_HASH), uid)));
+    c.bench_function("client_initialization", |b| b.iter(|| Client::new(Parameters::new(USE_OPRF, USE_SLOW_HASH), String::from(uid))));
 }
 
 pub fn server_initialization(c: &mut Criterion) {
@@ -32,9 +32,9 @@ pub fn server_register_start(c: &mut Criterion) {
     let client = Client::new(params, uid);
     let server = Server::new(params);
 
-    let (register_request, oprf_client_state) = client.register_start(password);
+    let (register_request, _) = client.register_start(password);
 
-    c.bench_function("server_register_start", |b| b.iter(|| server.register_start(black_box((register_request)))));
+    c.bench_function("server_register_start", |b| b.iter(|| server.register_start(black_box(register_request.clone()))));
 }
 
 pub fn client_register_finish(c: &mut Criterion) {
@@ -45,9 +45,9 @@ pub fn client_register_finish(c: &mut Criterion) {
     let server = Server::new(params);
 
     let (register_request, oprf_client_state) = client.register_start(password);
-    let (register_response, pre_register_secrets) = server.register_start(register_request);
+    let (register_response, _) = server.register_start(register_request);
 
-    c.bench_function("client_register_finish", |b| b.iter(|| client.register_finish(black_box(register_response), black_box(oprf_client_state))));
+    c.bench_function("client_register_finish", |b| b.iter(|| client.register_finish(black_box(register_response.clone()), black_box(oprf_client_state.clone()))));
 }
 
 pub fn server_register_finish(c: &mut Criterion) {
@@ -60,7 +60,7 @@ pub fn server_register_finish(c: &mut Criterion) {
     let (register_response, pre_register_secrets) = server.register_start(register_request);
     let register_finish = client.register_finish(register_response, oprf_client_state);
 
-    c.bench_function("server_register_finish", |b| b.iter(|| server.register_finish(black_box(register_finish), black_box(pre_register_secrets))));
+    c.bench_function("server_register_finish", |b| b.iter(|| server.register_finish(black_box(register_finish.clone()), black_box(pre_register_secrets.clone()))));
 }
 
 
