58215cf226e2e322895b10a2d521ca83c7181321
Julien Béguin <julien.beguin99@gmail.com>
2021-12-18 15:47:48 +0100

    register benchmark wip

---
+++ benches/khape.rs
@@ -0,0 +1,94 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use khape::*;
+
+const USE_OPRF: bool = false;
+const USE_SLOW_HASH: bool = false;
+
+pub fn client_initialization(c: &mut Criterion) {
+    let uid = String::from("username");
+
+    c.bench_function("client_initialization", |b| b.iter(|| Client::new(Parameters::new(USE_OPRF, USE_SLOW_HASH), uid)));
+}
+
+pub fn server_initialization(c: &mut Criterion) {
+    c.bench_function("server_initialization", |b| b.iter(|| Server::new(Parameters::new(USE_OPRF, USE_SLOW_HASH))));
+}
+
+
+pub fn client_register_start(c: &mut Criterion) {
+    let params = Parameters::new(USE_OPRF, USE_SLOW_HASH);
+    let uid = String::from("username");
+    let password = b"password";
+    let client = Client::new(params, uid);
+
+    c.bench_function("client_register_start", |b| b.iter(|| client.register_start(black_box(password))));
+}
+
+pub fn server_register_start(c: &mut Criterion) {
+    let params = Parameters::new(USE_OPRF, USE_SLOW_HASH);
+    let uid = String::from("username");
+    let password = b"password";
+    let client = Client::new(params, uid);
+    let server = Server::new(params);
+
+    let (register_request, oprf_client_state) = client.register_start(password);
+
+    c.bench_function("server_register_start", |b| b.iter(|| server.register_start(black_box((register_request)))));
+}
+
+pub fn client_register_finish(c: &mut Criterion) {
+    let params = Parameters::new(USE_OPRF, USE_SLOW_HASH);
+    let uid = String::from("username");
+    let password = b"password";
+    let client = Client::new(params, uid);
+    let server = Server::new(params);
+
+    let (register_request, oprf_client_state) = client.register_start(password);
+    let (register_response, pre_register_secrets) = server.register_start(register_request);
+
+    c.bench_function("client_register_finish", |b| b.iter(|| client.register_finish(black_box(register_response), black_box(oprf_client_state))));
+}
+
+pub fn server_register_finish(c: &mut Criterion) {
+    let params = Parameters::new(USE_OPRF, USE_SLOW_HASH);
+    let uid = String::from("username");
+    let password = b"password";
+    let client = Client::new(params, uid);
+    let server = Server::new(params);
+    let (register_request, oprf_client_state) = client.register_start(password);
+    let (register_response, pre_register_secrets) = server.register_start(register_request);
+    let register_finish = client.register_finish(register_response, oprf_client_state);
+
+    c.bench_function("server_register_finish", |b| b.iter(|| server.register_finish(black_box(register_finish), black_box(pre_register_secrets))));
+}
+
+
+
+
+
+
+// pub fn server_register_start(c: &mut Criterion) {
+//     let params = Parameters::new(USE_OPRF, USE_SLOW_HASH);
+//     let uid = String::from("username");
+//     let password = b"password";
+//     let client = Client::new(params, uid);
+//     let server = Server::new(params);
+//     let (register_request, oprf_client_state) = client.register_start(password);
+//     let (register_response, pre_register_secrets) = server.register_start(register_request);
+//     let register_finish = client.register_finish(register_response, oprf_client_state);
+//     let file_entry = server.register_finish(register_finish, pre_register_secrets);
+//
+//     c.bench_function("client_register_start", |b| b.iter(|| ));
+// }
+
+criterion_group!(
+    benches,
+    client_initialization,
+    server_initialization,
+    client_register_start,
+    server_register_start,
+    client_register_finish,
+    server_register_finish,
+    );
+criterion_main!(benches);
